86dc7155a05021a40c9fc09082902edbc6d26749
Reemabdu213 <11972@holbertonstudents.com>
2025-08-24 14:31:15 +0000

    Fix 6-size.c: wrap long lines; use %lu for sizeof(long long int)

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,16 +1,23 @@
+/*
+ * File: 6-size.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the size of various types based on
+ *        the computer it is compiled and run on..
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", (int)sizeof(char));
-	printf("Size of an int: %d byte(s)\n", (int)sizeof(int));
-	printf("Size of a long int: %ld byte(s)\n", (long int)sizeof(long int));
-	printf("Size of a long long int: %lld byte(s)\n", (long long int)sizeof(long long int));
-	printf("Size of a float: %d byte(s)\n", (int)sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 	return (0);
-}
+
