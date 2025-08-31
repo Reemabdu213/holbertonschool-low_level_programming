fbaaad31bc87a5b64198cf3946202d56c90608c2
Reemabdu213 <11972@holbertonstudents.com>
2025-08-31 16:51:50 +0000

    Task 5: Implement more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,16 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 
+/**
+ * more_numbers - Print numbers 10 times
+ * My function
+ * Return: Always 0 (Success)
+ */
 void more_numbers(void)
 {
-int i, j;
-for (i = 0; i < 10; i++)
-{
-for (j = 0; j <= 14; j++)
-{
-if (j >= 10)
-_putchar('1');
-_putchar(j % 10 + '0');
-}
-_putchar('\n');
-}
+    int a;
+    int i = 0;
+
+    while (i < 10)
+    {
+        for (a = 0; a < 15; a++)
+        {
+            if (a > 9)
+                _putchar('0' + (a / 10));
+            _putchar('0' + (a % 10));
+        }
+        _putchar('\n');
+        i++;
+    }
 }
