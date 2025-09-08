cc213317e24e71766525f59a937580bcf4bb1793
Reemabdu213 <11972@holbertonstudents.com>
2025-09-08 15:43:32 +0000

    Organize tasks into pointers_arrays_strings and add solutions

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,12 @@
 #include "main.h"
-#include <stddef.h>
+#include <stddef.h>  /* for NULL */
 
 /**
  * _strchr - locates a character in a string
- * @s: string to search in
+ * @s: pointer to the string to search in
  * @c: character to search for
  *
- * Return: pointer to first occurrence of c or NULL if not found
+ * Return: pointer to the first occurrence of c in s, or NULL if not found
  */
 char *_strchr(char *s, char c)
 {
