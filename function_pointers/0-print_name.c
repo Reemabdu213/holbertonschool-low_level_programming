339d01857f8db7b6935ef547dc7b4ffbf20b1ee4
Reemabdu213 <11972@holbertonstudents.com>
2025-09-21 20:52:34 +0000

    Add 0-print_name with Betty documentation

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * print_name - prints a name using the function passed as parameter
+ * @name: the name to print
+ * @f: pointer to a function that takes a char* and returns void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,10 +1,14 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#include <stddef.h>
-
-/* Prototypes (مطابقة لاسماء التاسكات) */
+/**
+ * print_name - prints a name using a function pointer
+ * @name: the name to be printed
+ * @f: pointer to a function that prints the name
+ */
 void print_name(char *name, void (*f)(char *));
+
+/* (optional for later tasks) */
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
