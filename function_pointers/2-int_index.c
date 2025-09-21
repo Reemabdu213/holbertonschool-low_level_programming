7796d0b35bb639fbec522f9b20579aef2f3d4aa4
Reemabdu213 <11972@holbertonstudents.com>
2025-09-21 20:42:50 +0000

    Implement 2-int_index.c and header; conforms to Betty

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,13 +1,14 @@
 #include "function_pointers.h"
 
 /**
- * int_index - searches for an integer
- * @array: array of integers
- * @size: number of elements in the array
- * @cmp: pointer to the function to compare values
+ * int_index - searches for an integer using a comparator function
+ * @array: pointer to the first element of the array
+ * @size: number of elements in @array
+ * @cmp: pointer to function used to compare values
  *
- * Return: index of the first element for which cmp does not return 0,
- *         -1 if no match or if size <= 0 or if array/cmp is NULL
+ * Return: index of first element for which cmp does not return 0,
+ *         or -1 if no element matches, or if size <= 0,
+ *         or if array/cmp is NULL.
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -21,5 +22,6 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
+
 	return (-1);
 }
--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,25 +1,11 @@
-i#ifndef FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
 
-/**
- * array_iterator - executes a function on each element of an array
- * @array: pointer to the array of integers
- * @size: size of the array
- * @action: pointer to the function used on each element
- */
+/* Prototypes (مطابقة لاسماء التاسكات) */
+void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
-
-/**
- * int_index - searches for an integer in an array
- * @array: pointer to the array of integers
- * @size: number of elements in the array
- * @cmp: pointer to the comparison function
- *
- * Return: index of the first element for which cmp does not return 0,
- *         -1 if no element matches or if size <= 0
- */
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
