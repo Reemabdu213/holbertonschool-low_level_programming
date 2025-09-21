df19a48780d79adf49730db332bca4c73eae7ddb
Reemabdu213 <11972@holbertonstudents.com>
2025-09-21 20:38:19 +0000

    Add correct int_index implementation

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
-#include <stddef.h>
 
 /**
- * int_index - searches for an integer in an array
- * @array: pointer to the array of integers
+ * int_index - searches for an integer
+ * @array: array of integers
  * @size: number of elements in the array
- * @cmp: pointer to the comparison function
+ * @cmp: pointer to the function to compare values
  *
  * Return: index of the first element for which cmp does not return 0,
- *         -1 if no element matches or if size <= 0
+ *         -1 if no match or if size <= 0 or if array/cmp is NULL
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
@@ -22,6 +21,5 @@ int int_index(int *array, int size, int (*cmp)(int))
 		if (cmp(array[i]) != 0)
 			return (i);
 	}
-
 	return (-1);
 }
