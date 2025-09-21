c29a254aeac2275f5e35e267622243acd13b95e0
Reemabdu213 <11972@holbertonstudents.com>
2025-09-21 20:54:46 +0000

    Add 0-print_name with Betty documentation and style fixes

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
 #include "function_pointers.h"
-#include <stddef.h>
 
 /**
  * print_name - prints a name using the function passed as parameter
  * @name: the name to print
  * @f: pointer to a function that takes a char* and returns void
+ *
+ * Return: Nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
