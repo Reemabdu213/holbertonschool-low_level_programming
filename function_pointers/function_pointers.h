ab03b1fe68264bfc234c157e1c2075d30db6bb63
Reemabdu213 <11972@holbertonstudents.com>
2025-09-21 20:30:00 +0000

    Update function_pointers.h with final prototypes

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,15 +1,25 @@
-#ifndef FUNCTION_POINTERS_H
+i#ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
 #include <stddef.h>
 
 /**
- * array_iterator - prototype for function that executes
- *                  a function on each element of an array
+ * array_iterator - executes a function on each element of an array
  * @array: pointer to the array of integers
  * @size: size of the array
  * @action: pointer to the function used on each element
  */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/**
+ * int_index - searches for an integer in an array
+ * @array: pointer to the array of integers
+ * @size: number of elements in the array
+ * @cmp: pointer to the comparison function
+ *
+ * Return: index of the first element for which cmp does not return 0,
+ *         -1 if no element matches or if size <= 0
+ */
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
