a12d1f4243ad37d2d18a256b9857e475572e6266
Reemabdu213 <11972@holbertonstudents.com>
2025-09-22 14:12:14 +0000

    Add print_numbers function

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,6 +3,38 @@
 
 #include <stdarg.h>
 
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: number of integers to sum
+ *
+ * Return: sum of numbers, or 0 if n == 0
+ */
 int sum_them_all(const unsigned int n, ...);
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: string to be printed between numbers
+ * @n: number of integers passed to the function
+ *
+ * Return: void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...);
+
+/**
+ * print_strings - prints strings, followed by a new line
+ * @separator: string to be printed between strings
+ * @n: number of strings passed to the function
+ *
+ * Return: void
+ */
+void print_strings(const char *separator, const unsigned int n, ...);
+
+/**
+ * print_all - prints anything (numbers, chars, strings, floats)
+ * @format: list of types of arguments passed to the function
+ *
+ * Return: void
+ */
+void print_all(const char * const format, ...);
 
 #endif /* VARIADIC_FUNCTIONS_H */
