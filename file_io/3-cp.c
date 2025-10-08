c0064db372b8b12cbf5ed7fb7436e74f57957bbb
Reemabdu213 <11972@holbertonstudents.com>
2025-10-08 17:12:23 +0000

    3-cp: fix read() error path (exit 98) and add Betty docs

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -2,29 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
 
 /**
- * print_error_and_exit - print error message to stderr and exit with code
- * @code: exit code
- * @fmt:  format string
- * @arg:  single string/int argument for the format
+ * print_exit - Print an error message to stderr then exit with code.
+ * @code: exit status to use.
+ * @fmt:  printf-like format string.
+ * @sarg: string argument for %s (or NULL if not used).
+ * @iarg: int argument for %d   (used حين نطبع fd في close).
+ *
+ * Description: Small helper to avoid code duplication and keep Betty happy.
  */
-static void print_error_and_exit(int code, const char *fmt, int arg_is_fd, const char *arg_s, int arg_fd)
+static void print_exit(int code, const char *fmt, const char *sarg, int iarg)
 {
-	if (arg_is_fd)
-		dprintf(STDERR_FILENO, fmt, arg_fd);
+	if (sarg)
+		dprintf(STDERR_FILENO, fmt, sarg);
 	else
-		dprintf(STDERR_FILENO, fmt, arg_s);
+		dprintf(STDERR_FILENO, fmt, iarg);
 	exit(code);
 }
 
 /**
- * main - copies the content of a file to another
- * @ac: argument count
- * @av: argument values
+ * main - Copy the content of a file to another file.
+ * @ac: argument count.
+ * @av: argument vector (av[1]=file_from, av[2]=file_to).
  *
- * Return: 0 on success, exits with specific codes on error
+ * Return: 0 on success. Exits with:
+ * 97 on wrong usage, 98 on read error, 99 on write error,
+ * 100 on close error.
  */
 int main(int ac, char **av)
 {
@@ -32,47 +36,47 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-		print_error_and_exit(97, "Usage: cp file_from file_to\n", 0, NULL, 0);
+		print_exit(97, "Usage: cp file_from file_to\n", NULL, 0);
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error_and_exit(98, "Error: Can't read from file %s\n", 0, av[1], 0);
+		print_exit(98, "Error: Can't read from file %s\n", av[1], 0);
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		print_error_and_exit(99, "Error: Can't write to %s\n", 0, av[2], 0);
+		print_exit(99, "Error: Can't write to %s\n", av[2], 0);
 	}
 
 	while (1)
 	{
 		r = read(fd_from, buf, sizeof(buf));
-		if (r == -1) /* read failure -> 98 */
+		if (r == -1) /* لازم نوقف هنا ونطلع 98 قبل أي write */
 		{
 			close(fd_to);
 			close(fd_from);
-			print_error_and_exit(98, "Error: Can't read from file %s\n", 0, av[1], 0);
+			print_exit(98, "Error: Can't read from file %s\n", av[1], 0);
 		}
 		if (r == 0) /* EOF */
 			break;
 
 		w = write(fd_to, buf, r);
-		if (w == -1 || w != r) /* write failure -> 99 */
+		if (w == -1 || w != r)
 		{
 			close(fd_to);
 			close(fd_from);
-			print_error_and_exit(99, "Error: Can't write to %s\n", 0, av[2], 0);
+			print_exit(99, "Error: Can't write to %s\n", av[2], 0);
 		}
 	}
 
 	c1 = close(fd_from);
 	if (c1 == -1)
-		print_error_and_exit(100, "Error: Can't close fd %d\n", 1, NULL, fd_from);
+		print_exit(100, "Error: Can't close fd %d\n", NULL, fd_from);
 
 	c2 = close(fd_to);
 	if (c2 == -1)
-		print_error_and_exit(100, "Error: Can't close fd %d\n", 1, NULL, fd_to);
+		print_exit(100, "Error: Can't close fd %d\n", NULL, fd_to);
 
 	return (0);
 }
