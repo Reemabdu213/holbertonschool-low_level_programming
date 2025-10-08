71d38881c56bbe42d739be5dfb6bf428385cc141
Reemabdu213 <11972@holbertonstudents.com>
2025-10-08 17:17:21 +0000

    Fix: correct read() error handling (exit 98) + Betty clean

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -4,13 +4,13 @@
 #include <fcntl.h>
 
 /**
- * die - print error to stderr then exit with code
- * @code: exit status (97/98/99/100)
- * @fmt:  printf-like format
- * @s:    string arg for %s (NULL if using %d)
- * @fd:   int arg for %d (used with close errors)
+ * error_exit - Prints an error message to stderr and exits.
+ * @code: Exit status (97, 98, 99, or 100)
+ * @fmt: Format string (e.g. "Error: Can't read from file %s\n")
+ * @s: String argument for %s (NULL if not used)
+ * @fd: Integer argument for %d (used when closing fd)
  */
-static void die(int code, const char *fmt, const char *s, int fd)
+static void error_exit(int code, const char *fmt, const char *s, int fd)
 {
 	if (s)
 		dprintf(STDERR_FILENO, fmt, s);
@@ -20,12 +20,11 @@ static void die(int code, const char *fmt, const char *s, int fd)
 }
 
 /**
- * main - copy file_from into file_to (1024 bytes per read)
- * @ac: argc
- * @av: argv
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count.
+ * @av: Argument vector.
  *
- * Return: 0 on success, exits with:
- * 97 usage, 98 read error, 99 write error, 100 close error
+ * Return: 0 on success.
  */
 int main(int ac, char **av)
 {
@@ -33,27 +32,27 @@ int main(int ac, char **av)
 	char buf[1024];
 
 	if (ac != 3)
-		die(97, "Usage: cp file_from file_to\n", NULL, 0);
+		error_exit(97, "Usage: cp file_from file_to\n", NULL, 0);
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		die(98, "Error: Can't read from file %s\n", av[1], 0);
+		error_exit(98, "Error: Can't read from file %s\n", av[1], 0);
 
 	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		die(99, "Error: Can't write to %s\n", av[2], 0);
+		error_exit(99, "Error: Can't write to %s\n", av[2], 0);
 	}
 
-	while ((r = read(fd_from, buf, sizeof(buf))) > 0)
+	while ((r = read(fd_from, buf, 1024)) > 0)
 	{
 		w = write(fd_to, buf, r);
 		if (w == -1 || w != r)
 		{
 			close(fd_from);
 			close(fd_to);
-			die(99, "Error: Can't write to %s\n", av[2], 0);
+			error_exit(99, "Error: Can't write to %s\n", av[2], 0);
 		}
 	}
 
@@ -61,17 +60,16 @@ int main(int ac, char **av)
 	{
 		close(fd_from);
 		close(fd_to);
-		die(98, "Error: Can't read from file %s\n", av[1], 0);
+		error_exit(98, "Error: Can't read from file %s\n", av[1], 0);
 	}
 
 	c1 = close(fd_from);
 	if (c1 == -1)
-		die(100, "Error: Can't close fd %d\n", NULL, fd_from);
+		error_exit(100, "Error: Can't close fd %d\n", NULL, fd_from);
 
 	c2 = close(fd_to);
 	if (c2 == -1)
-		die(100, "Error: Can't close fd %d\n", NULL, fd_to);
+		error_exit(100, "Error: Can't close fd %d\n", NULL, fd_to);
 
 	return (0);
 }
-
